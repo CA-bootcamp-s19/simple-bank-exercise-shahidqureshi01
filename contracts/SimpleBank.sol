0731bc5dfaa75ac6cc98bffc595b1e2bdad477d8
Shahid Qureshi <shahidsabir21@hotmail.com>
2019-07-25 17:30:03 +0500

    added code to wIthDrawa() function

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -95,6 +95,11 @@ contract SimpleBank {
            Subtract the amount from the sender's balance, and try to send that amount of ether
            to the user attempting to withdraw. 
            return the user's balance.*/
+        if(withdrawAmount <= balances[msg.sender]) {
+            balances[msg.sender] -= withdrawAmount;
+            msg.sender.send(withdrawAmount);
+        }
+        return balances[msg.sender];
     }
 
 }
