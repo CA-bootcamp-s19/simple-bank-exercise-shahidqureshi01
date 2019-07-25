c8c563d48841c507bfd130a2753a3b78c0b9d1b2
Shahid Qureshi <shahidsabir21@hotmail.com>
2019-07-25 17:10:18 +0500

    added code to function headers and body

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -34,7 +34,7 @@ contract SimpleBank {
     /* Create an event called LogWithdrawal */
     /* Add 3 arguments for this event, an accountAddress, withdrawAmount and a newBalance */
 
-    event logWithdrawal(address accountAddress, uint withdrawAmount, uint newBalance)
+    event logWithdrawal(address accountAddress, uint withdrawAmount, uint newBalance);
     //
     // Functions
     //
@@ -42,6 +42,7 @@ contract SimpleBank {
     /* Use the appropriate global variable to get the sender of the transaction */
     constructor() public {
         /* Set the owner to the creator of this contract */
+        owner = msg.sender;
     }
 
     // Fallback function - Called if other functions don't match call or
@@ -57,14 +58,18 @@ contract SimpleBank {
     /// @return The balance of the user
     // A SPECIAL KEYWORD prevents function from editing state variables;
     // allows function to run locally/off blockchain
-    function getBalance() public returns (uint) {
+    function getBalance(address account) public view returns (uint) {
         /* Get the balance of the sender of this transaction */
+        return balances[account];
     }
 
     /// @notice Enroll a customer with the bank
     /// @return The users enrolled status
     // Emit the appropriate event
-    function enroll() public returns (bool){
+    function enroll(address account) public returns (bool){
+        enrolled[account] = true;
+        emit LogEnrolled(account);
+        return true;
     }
 
     /// @notice Deposit ether into bank
@@ -73,9 +78,11 @@ contract SimpleBank {
     // Use the appropriate global variables to get the transaction sender and value
     // Emit the appropriate event    
     // Users should be enrolled before they can make deposits
-    function deposit() public returns (uint) {
+    function deposit() public payable returns (uint) {
         /* Add the amount to the user's balance, call the event associated with a deposit,
           then return the balance of the user */
+        balances[msg.sender] += msg.value;
+        emit LogDepositMade(msg.sender, msg.value);
     }
 
     /// @notice Withdraw ether from bank
